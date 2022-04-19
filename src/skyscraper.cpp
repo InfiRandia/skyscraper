efc1151b7d43e2bf2a2710fe8acc0f1090769543
InfiRandia <41839345+InfiRandia@users.noreply.github.com>
2022-04-19 23:02:36 +0200

    Update skyscraper.cpp

--- src/skyscraper.cpp
+++ src/skyscraper.cpp
@@ -832,7 +832,8 @@ void Skyscraper::loadConfig(const QCommandLineParser &parser)
     config.scummIni = settings.value("scummIni").toString();
   }
   // Check for command line platform here, since we need it for 'platform' config.ini entries
-  if(parser.isSet("p") && Platform::getPlatforms().contains(parser.value("p"))) {
+  // '_' is seen as a subcategory of the selected platform
+  if(parser.isSet("p") && Platform::getPlatforms().contains(parser.value("p").split('_').first())) {
     config.platform = parser.value("p");
   } else {
     if((!parser.isSet("flags") && parser.value("flags") != "help") &&
